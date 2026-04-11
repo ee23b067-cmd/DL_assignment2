7d070fc0bef9ae1c10ca83c8c4ce320c74638bf2
SADAM RAMAKRISHNA <ee23b067@smail.iitm.ac.in>
2026-04-11 16:53:15 +0530

    Update multitask.py

--- models/multitask.py
+++ models/multitask.py
@@ -73,7 +73,7 @@ class MultiTaskPerceptionModel(nn.Module):
         load_weights(unet_model, unet_path)
 
         # Shared backbone
-        self.backbone = unet_model.encoder
+        self.backbone = classifier_model.encoder
 
 
         # Heads
